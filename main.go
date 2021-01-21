2934c0903ac2571e03d69f6121134a3df51cda31
hubenchang0515 <hubenchang0515@outlook.com>
2021-01-21 16:50:36 +0800

    add init command

--- main.go
+++ main.go
@@ -16,19 +16,23 @@ const (
 
 func main() {
 	err := work()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+	if err != nil && os.IsNotExist(err) {
+		fmt.Printf("Here is not a go-test-ignore project, please run '%s init'\n", os.Args[0])
+	} else if err != nil {
+		fmt.Fprintf(os.Stderr, "[ERROR] %v\n", err)
 	}
 }
 
 // 打印帮助信息
 func help(exe string) {
-	fmt.Printf("Usage   : %s submit\n", exe)
+	fmt.Printf("Usage   : %s init\n", exe)
+	fmt.Printf("          %s submit\n", exe)
 	fmt.Printf("          %s add <file-path>\n", exe)
 	fmt.Printf("          %s del <file-path>\n", exe)
 	fmt.Printf("          %s set-flag <build-flag>\n", exe)
 	fmt.Println()
-	fmt.Printf("Example : %s add main.go\n", exe)
+	fmt.Printf("Example : %s init\n", exe)
+	fmt.Printf("          %s add main.go\n", exe)
 	fmt.Printf("          %s add file_modify.go\n", exe)
 	fmt.Printf("          %s set-flag !GO_TEST\n", exe)
 	fmt.Printf("          %s submit\n", exe)
@@ -36,30 +40,42 @@ func help(exe string) {
 
 // 处理函数
 func work() error {
+	// 参数检查
+	if len(os.Args) <= 1 {
+		help(os.Args[0])
+		return nil
+	}
+
+	if os.Args[1] == "init" {
+		config := NewIgnoreConfig()
+		err := config.Write(defaultConfigFile)
+		return err
+	}
+
 	// 加载配置
 	config := NewIgnoreConfig()
-	config.Load(path.Join(".", defaultConfigFile))
+	err := config.Load(path.Join(".", defaultConfigFile))
+	if err != nil {
+		return err
+	}
 
-	// 参数检查
-	if len(os.Args) == 1 {
-		help(os.Args[0])
-	} else if os.Args[1] == "add" && len(os.Args) > 2 {
+	if os.Args[1] == "add" && len(os.Args) > 2 {
 		for _, file := range os.Args[2:] {
 			if !strings.HasSuffix(file, ".go") {
-				fmt.Fprintf(os.Stderr, "skip '%s' not a go file\n", file)
+				fmt.Fprintf(os.Stderr, "[WARNING] Skip '%s' not a go file\n", file)
 				continue
 			}
-			fmt.Fprintf(os.Stderr, "add '%s'\n", file)
+			fmt.Printf("[Info] Add '%s'\n", file)
 			config.AddIgnore(file)
 		}
 		return config.Write(defaultConfigFile)
 	} else if os.Args[1] == "del" && len(os.Args) > 2 {
 		for _, file := range os.Args[2:] {
 			if !strings.HasSuffix(file, ".go") {
-				fmt.Fprintf(os.Stderr, "skip '%s' not a go file\n", file)
+				fmt.Fprintf(os.Stderr, "[WARNING] Skip '%s' not a go file\n", file)
 				continue
 			}
-			fmt.Fprintf(os.Stderr, "del '%s'\n", file)
+			fmt.Printf("[Info] Del '%s'\n", file)
 			config.DelIgnore(file)
 		}
 		return config.Write(defaultConfigFile)
@@ -104,16 +120,21 @@ func scanDir(dir string, handler func(filename string) error) error {
 // 创建文件处理器
 func createFileHandler(config *IgnoreConfig) func(string) error {
 	return func(filename string) error {
+		var err error = nil
 		flag := config.BuildFlag
 		if !strings.HasSuffix(filename, ".go") {
 			// fmt.Printf("Skip '%s' : not a go file.\n", filename)
 		} else if config.ShouldIgnore(filename) {
-			fmt.Printf("Add '%s' into '%s'\n", flag, filename)
-			return AddBuildFlagToFile(filename, flag)
+			err = AddBuildFlagToFile(filename, flag)
+			if err == nil {
+				fmt.Printf("[Info] Add '%s' into '%s'\n", flag, filename)
+			}
 		} else {
-			fmt.Printf("Remove '%s' from '%s'\n", flag, filename)
-			return DelBuildFlagFromFile(filename, flag)
+			err = DelBuildFlagFromFile(filename, flag)
+			if err == nil {
+				fmt.Printf("[Info] Remove '%s' from '%s'\n", flag, filename)
+			}
 		}
-		return nil
+		return err
 	}
 }
